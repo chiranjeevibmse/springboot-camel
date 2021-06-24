36801c0ca08af2d9bc6b19f264fedd7be5491f60
Chiranjeevi GK <chiranjeevibmse@gmail.com>
2021-06-24 17:14:01 +0530

    added initial project

---
+++ copy-file/src/main/java/com/chiru/boot/camel/copyfile/CopyFileApplication.java
@@ -0,0 +1,13 @@
+package com.chiru.boot.camel.copyfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopyFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopyFileApplication.class, args);
+	}
+
+}
---
+++ copy-file/src/test/java/com/chiru/boot/camel/copyfile/CopyFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiru.boot.camel.copyfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopyFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
